98e5b61a01d96227ef916d0a84e4be2cd8995eab
“jason” <wangjianmas@gmail.com>
2021-06-12 23:45:51 +0800

    sql 0612

--- ORACLE/oracle_sql.sql
+++ ORACLE/oracle_sql.sql
@@ -39,4 +39,56 @@ WHERE SAL BETWEEN 800 AND 1500;
 SELECT ENAME , SAL FROM EMP 
 WHERE SAL >=800 AND SAL<=1500;
 
-SELECT ENAME 
+
+select ename, sal , comm from emp 
+where comm is null;
+select ename, sal , comm from emp 
+where comm is not null;
+
+select ename, sal, comm from emp where 
+SAL in (800 ,1500, 2000);
+
+select ename, sal, comm from emp where 
+ename in ('SMITH','KING','ABC');
+
+select sysdate from emp;
+
+select ename, sal, HIREDATE from emp 
+where HIREDATE > '20-2月-81';
+
+desc emp;
+
+select hiredate from emp;
+
+select ename, sal from emp
+where deptno = 10 
+and sal>1000;
+
+select ename, sal from emp
+where deptno = 10 
+or sal>1000;
+
+select ename, sal from emp
+where sal not in (800,1500);
+
+
+select ename FROM emp where ename like '%ALL%';
+//secend alm is A
+select ename FROM emp where ename like '_A%';
+
+select ename FROM emp where ename like '%\%%';
+
+select ename FROM emp where ename like '%$%%' 
+escape '$';
+
+
+select * from dept order by DEPTNO;
+select * from dept order by deptno desc;
+select deptno , ename from emp ;
+select deptno , ename from emp order by deptno asc;
+
+select deptno , ename from emp order by deptno asc;
+select deptno , ename from emp where
+deptno <> 10 order by deptno desc;
+
+select deptno , ename from emp order by deptno asc， ename desc;
